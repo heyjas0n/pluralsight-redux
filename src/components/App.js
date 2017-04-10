a12ec53bcd2d63c87f5e5ba9131f43c84b60450a
Jason <jasonvex@gmail.com>
2017-04-10 17:31:42 -0500

    Async status and error handling. Hide preloader based on status

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,31 @@
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
+import {connect} from 'react-redux';
 /*
-  We need a component to load on every page and wrap up all the compoonents that we create.
-  Thus, we make App.js
-    react-router will be passing child copmonents as props onto App
-*/
+ We need a component to load on every page and wrap up all the compoonents that we create.
+ Thus, we make App.js
+ react-router will be passing child copmonents as props onto App
+ */
 class App extends React.Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <Header/>
-        {this.props.children}
-      </div>
-    );
-  }
+    render () {
+        return (
+        <div className="container-fluid">
+            <Header loading = {this.props.loading}/>
+            {this.props.children}
+        </div>
+        );
+    }
 }
 
 App.proptypes = {
-  children: PropTypes.object.isRequired
+    children: PropTypes.object.isRequired,
+    loading: PropTypes.bool.isRequired
 };
 
-export default App;
+function mapStateToProps (state, ownProps) {
+    return {
+        loading: state.ajaxCallsInProgress > 0
+    };
+}
+
+export default connect(mapStateToProps)(App);
